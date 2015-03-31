16308053a57558f526dca5789ef7755bd38e0e6d
thelastpenguin <thelastpenguin212@gmail.com>
2015-03-31 00:42:40 -0700

    fix(pnet): didn't realize it was even broken... apparently it was

--- lua/includes/modules/pnet.lua
+++ lua/includes/modules/pnet.lua
@@ -1,34 +1,39 @@
 if SERVER then
-  util.AddNetworkString('pnet_Ready')
-  local ready = { }
+  AddCSLuaFile()
+
+  local ready = {}
+
   hook.Add('PlayerDisconnected', 'pnet.PlayerDisconnected', function(pl)
     ready[pl] = nil
   end)
+
   net.waitForPlayer = function(pl, func)
     if ready[pl] == true then
       func()
     else
-      if not (ready[pl]) then
-        ready[pl] = { }
+      if ready[pl] == nil then
+        ready[pl] = {}
       end
       table.insert(ready[pl], func)
     end
   end
-  return net.Receive('pnet_Ready', function(_, pl)
-    if ready[pl] == true or ready[pl] == nil then
-      return 
-    end
-    local _list_0 = ready[pl]
-    for _index_0 = 1, #_list_0 do
-      local func = _list_0[_index_0]
-      func()
-    end
+
+  util.AddNetworkString('pnet_Ready')
+  net.Receive('pnet_Ready', function(_, pl)
+    local todo = ready[pl]
+    if todo == true then return end    
     ready[pl] = true
+    if todo == nil then return end
+
+    for k,v in pairs(todo)do
+      v()
+    end
   end)
+
 else
-  return hook.Add('Think', 'pnet.waitForPlayer', function()
+  hook.Add('Think', 'pnet.WaitForPlayer', function()
     if IsValid(LocalPlayer()) then
-      hook.Remove('Think', 'pnet.waitForPlayer')
+      hook.Remove('Think', 'pnet.WaitForPlayer')
       net.Start('pnet_Ready')
       net.SendToServer()
     end
