fc2947e33a8a1ef081fec51cd7256a3aea8dec9c
unknown <zach@superiorservers.co>
2015-05-25 10:54:43 -0800

    geoip module

---
+++ lua/includes/modules/geoip.lua
@@ -0,0 +1,35 @@
+-- I'll expand this.. some day
+require('plib')
+
+geoip 				= {}
+
+local geoip 		= geoip
+local error 		= p.error
+local http_fetch 	= http.Fetch
+local json_to_table = util.JSONToTable
+
+local failures		 = 5
+local result_cache 	= {}
+
+
+function geoip.Get(ip, cback)
+	if result_cache[ip] then
+		cback(result_cache[ip])
+	else
+		http_fetch('http://freegeoip.net/json/' .. ip, function(b)
+			if (b == '404 page not found') then
+				error('GeoIP: Failed to lookup ip: ' .. ip)
+			else
+				local res = json_to_table(b)
+				failures = 0
+				result_cache[ip] = res
+				cback(res)
+			end
+		end, function()
+			if (failures <= 5) then 
+				failures = failures + a
+				geoip.Get(ip, cback)
+			end
+		end)
+	end
+end
