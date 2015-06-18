b3744a0d8779d90dc759ad2c1bc571d7b45d1b0e
unknown <zach@superiorservers.co>
2015-06-17 22:24:04 -0700

    Misc

--- lua/includes/modules/speedy.lua
+++ lua/includes/modules/speedy.lua
@@ -3,22 +3,35 @@
 
 -- Net
 do 
-	local net 		= net
 	local IsValid 	= IsValid
 	local Entity 	= Entity
+	local Color 	= Color
+	local WriteUInt = net.WriteUInt
+	local ReadUInt 	= net.ReadUInt
 
 	function net.WriteEntity(ent)
 		if IsValid(ent) then 
-			net.WriteUInt(ent:EntIndex(), 12)
+			WriteUInt(ent:EntIndex(), 12)
 		else
-			net.WriteUInt(0, 12)
+			WriteUInt(0, 12)
 		end
 	end
 
 	function net.ReadEntity()
-		local i = net.ReadUInt(12)
+		local i = ReadUInt(12)
 		if not i then return end
 		return Entity(i)
+	end
+
+	function net.WriteColor(c)
+		WriteUInt(c.r, 8)
+		WriteUInt(c.g, 8)
+		WriteUInt(c.b, 8)
+		WriteUInt(c.a, 8)
+	end
+
+	function net.ReadColor()
+		return Color(ReadUInt(8), ReadUInt(8), ReadUInt(8), ReadUInt(8))
 	end
 end
 
@@ -49,12 +62,13 @@ do
 				x = x, 
 				y = y
 			}
+			return x, y
 		end
 		   
 		return SizeCache[Font][text].x, SizeCache[Font][text].y
 	end
 	 
 	timer.Create('PurgeFontCache', 1200, 0, function()
-		SizeCachehe = {}
+		SizeCache = {}
 	end)
 end
