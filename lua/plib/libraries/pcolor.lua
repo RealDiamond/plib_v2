0541354be467d1e4ab41a72db937ee46ce48880a
unknown <zach@superiorservers.co>
2015-07-11 01:17:26 -0700

    change color back to pcolor

--- lua/plib/libraries/pcolor.lua
+++ lua/plib/libraries/pcolor.lua
@@ -1,4 +1,4 @@
-color = {
+pcolor = {
 	White 	= Color(255,255,255), -- todo, more colors?
 	Black 	= Color(0,0,0),
 	Red 	= Color(255,0,0),
@@ -14,27 +14,27 @@ local string_match 	= string.match
 local bit_band		= bit.band
 local bit_rshift 	= bit.rshift
 
-function color.ToHex(col)
+function pcolor.ToHex(col)
 	return string_format('#%02X%02X%02X', col.r, col.g, col.b)
 end
 
-function color.FromHex(hex)
+function pcolor.FromHex(hex)
 	local r, g, b = string_match(hex, '#(..)(..)(..)')
 	return Color(tonumber(r, 16), tonumber(g, 16), tonumber(b, 16))
 end
 
-function color.EncodeRGB(col)
+function pcolor.EncodeRGB(col)
 	return (col.r * 0x100 + col.g) * 0x100 + col.b
 end
 
-function color.DecodeRGB(num)
+function pcolor.DecodeRGB(num)
 	return Color(bit_band(bit_rshift(num, 16), 0xFF), bit_band(bit_rshift(num, 8), 0xFF), bit_band(num, 0xFF))
 end
 
-function color.EncodeRGBA(col)
+function pcolor.EncodeRGBA(col)
 	return ((col.a * 0x100 + col.r) * 0x100 + col.g) * 0x100 + col.b
 end
 
-function color.DecodeRGBA(num)
+function pcolor.DecodeRGBA(num)
 	return Color(bit_band(rshift(num, 16), 0xFF), bit_band(rshift(num, 8), 0xFF), bit_band(num, 0xFF), bit_band(rshift(num, 24), 0xFF))
 end
