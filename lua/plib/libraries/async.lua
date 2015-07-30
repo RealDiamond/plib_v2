ed7373e6c3acccae138a6b6e8b02b4e4617d2a8b
thelastpenguin <thelastpenguin212@gmail.com>
2015-07-30 14:08:41 +0200

    added line counter and started to rewrite async without using moonscript. This will break some of my really old projects

--- lua/plib/libraries/async.lua
+++ lua/plib/libraries/async.lua
@@ -1,100 +1,55 @@
-async = async or { }
-local p = p
-async.parallel = function(tasks, onFinish)
-  if onFinish == nil then
-    onFinish = p.fn.noop
-  end
-  local todo = #tasks
-  for _index_0 = 1, #tasks do
-    local task = tasks[_index_0]
-    local canRun = true
-    task(function()
-      if canRun then
-        canRun = false
-        todo = todo - 1
+async = {}
+
+function async.parallel(tasks, cb)
+  local todo = #task
+
+  for k, task in ipairs(tasks) do
+    local wasCalled = false
+
+    task(function(err)
+      if wasCalled then
+        error('callback got called more than once')
+      end
+      wasCalled = true
+
+      if err then
+        cb(err)
+        cb = function() end
+      end
+
+      todo = todo - 1
+
+      if todo == 0 then
+        cb()
       end
     end)
+
   end
+
 end
-async.series = function(tasks, onFinish)
-  if onFinish == nil then
-    onFinish = p.fn.noop
-  end
-  if #tasks > 0 then
-    local canCall = true
-    local run
-    run = function()
-      local i = i + 1
-      return tasks[i](function()
-        if canCall then
-          canCall = false
-          return run()
+
+function async.series(tasks, cb)
+  
+  function iterate(index)
+    if not tasks[index] then
+      cb()
+    else
+      local wasCalled = false 
+      tasks(function(err)
+        if wasCalled then
+          error 'callback was called more than once'
+        end
+        wasCalled = true
+
+        if err then
+          cb(err)
+          cb = function() end
         else
-          return error('async series fn called back more than once')
+          iterate(index + 1)
         end
       end)
     end
-  else
-    return onFinish()
   end
-end
-async.eachParallel = function(tasks, work, onFinish)
-  if onFinish == nil then
-    onFinish = p.fn.noop
-  end
-  local todo = #tasks
-  for _index_0 = 1, #tasks do
-    local task = tasks[_index_0]
-    local canRun = true
-    work(task, function()
-      if canRun then
-        canRun = false
-        todo = todo - 1
-      end
-    end)
-  end
-end
-async.eachSeries = function(tasks, work, onFinish)
-  if onFinish == nil then
-    onFinish = p.fn.noop
-  end
-  if #tasks > 0 then
-    local i = 1
-    local fn
-    fn = function()
-      i = i + 1
-      if tasks[i] then
-        return work(tasks[i], fn)
-      else
-        onFinish()
-        onFinish = p.fn.noop
-      end
-    end
-    return work(tasks[1], fn)
-  else
-    return onFinish()
-  end
-end
-async.worker = function(data, rate, work, onFinish)
-  if onFinish == nil then
-    onFinish = p.fn.noop
-  end
-  if #tasks > 0 then
-    local i = 1
-    local fn
-    fn = function()
-      i = i + 1
-      if tasks[i] then
-        return work(tasks[i], fn)
-      else
-        onFinish()
-        onFinish = p.fn.noop
-      end
-    end
-    for i = 1, math.min(#data, rate) do
-      work(tasks[1], fn)
-    end
-  else
-    return onFinish()
-  end
+
+  iterate(1)
 end
