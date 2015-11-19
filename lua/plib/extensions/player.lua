ad8084333a56da6fdc1a2041eeab942b5c2c845f
aStonedPenguin <zach@superiorservers.co>
2015-11-18 18:25:33 -0900

    utils

--- lua/plib/extensions/player.lua
+++ lua/plib/extensions/player.lua
@@ -1,3 +1,16 @@
+function player.FindByInfo(info)
+	info = tostring(info)
+	for _, pl in ipairs(player.GetAll()) do
+		if (info == pl:SteamID()) then
+			return pl
+		elseif (info == pl:SteamID64()) then
+			return pl
+		elseif string.find(string.lower(pl:Name()), string.lower(info), 1, true) ~= nil then
+			return pl
+		end
+	end
+end
+
 local PLAYER = FindMetaTable('Player')
 
 function PLAYER:Timer(name, time, reps, callback)
--- lua/plib_unfinished/cmd.lua
+++ lua/plib_unfinished/cmd.lua
@@ -6,7 +6,7 @@ cmd = setmetatable({}, {
 		__call = function(self)
 			return self
 		end
-	})
+	}),
 })
 
 local cmd_mt = {
@@ -18,12 +18,89 @@ cmd_mt.__index 	= cmd_mt
 cmd_mt.__concat = cmd_mt.__tostring
 _R.cmd 			= cmd_mt
 
+local concommands = {}
+local params = {}
 
+
+-- Parsing
+function cmd.AddParam(name, parse, autocomplete)
+	local id = #params + 1
+	cmd[name:upper()] = id
+	params[id] = {
+		Parse = parse,
+		AutoComplete = autocomplete
+	}
+	return id
+end
+
+function cmd.ParseString(caller, cmd, arg_string)
+	local args = string.ExplodeQuotes(arg_string)
+
+	if (#args < #cmd:GetParams()) then
+		hook.Call('cmd.OnCommandError', nil, caller, cmd, 'To many arguments!', args)
+	end
+
+	local parsed_args = {}
+	for k, v in ipairs(cmd:GetParams()) do
+		local arg, err, used = params[v.Type].Parse(caller, cmd, args[1], arg, k)
+
+		if (arg == false) then
+			hook.Call('cmd.OnCommandError', nil, caller, cmd, err, args)
+		elseif (hook.Call('cmd.ParsedArgument', nil, caller, cmd, arg) ~= nil) then 
+			break 
+		end
+
+		for i = 1, (used or 1) do
+			table.remove(args, i)
+		end
+
+		parsed_args[#parsed_args + 1] = arg
+	end
+end
+
+-- Defualt parsers
+local function playercomplete(cmd, arg, step)
+	local ret = {}
+	if (arg ~= nil) and string.IsSteamID(arg) then 
+		ret = {arg}
+	else
+		for _, pl in ipairs(player.GetAll()) do
+			if (arg ~= nil) and string.find(pl:Name():lower(), arg:lower()) then
+				ret[#ret + 1] = pl:Name()
+			elseif (arg == nil) then
+				ret[#ret + 1] = pl:Name()
+			end
+		end
+	end
+	return ret
+end
+cmd.AddParam('PLAYER_STEAMID', function(caller, cmd, arg, args, step)
+	local result = player.FindByInfo(arg)
+	if (result == nil) and (not string.IsSteamID(arg)) then
+		return false, 'Player \'' ..  arg .. '\' not found!'
+	end
+	return true, result
+end, playercomplete)
+
+cmd.AddParam('PLAYER_ENTITY', function(caller, cmd, arg, args, step)
+	local result = player.FindByInfo(arg)
+	if (result == nil) then
+		return false, 'Player \'' ..  arg .. '\' not found!'
+	end
+	return true, result
+end, playercomplete)
+
+
+
+
+
+-- Commands
 function cmd.Add(name, callback)
 	local c = setmetatable({
 		Name  		= name:lower():gsub(' ', ''),
 		NiceName 	= name,
 		Params		= {},
+		CanRun 		= function() return true end
 		Callback	= callback or function() end
 	}, cmd_mt)
 	cmd.GetTable[c.Name] = c
@@ -38,17 +115,16 @@ function cmd.Remove(name)
 	cmd.GetTable[name] = nil
 end
 
-function cmd.Run(pl, name, ...)
-	if (not hook.Run('cmd.CanRunCommand', pl, cmd, msg, ...)) then return end
-
-	-- Parse shit here. maybe a whole new lib for that??
-
+function cmd.Run(caller, name, str_args)
 	local cmd = cmd.Get(name)
-	local succ, msg = cmd.Callback(...)
-	if (not succ) and msg then
-		hook.Run('cmd.OnCommandError', pl, cmd, msg, ...)
-	elseif (succ) and msg then
-		hook.Run('cmd.OnCommandRun', pl, cmd, msg, ...)
+	local args = cmd.ParseString(caller, cmd, str_args)
+
+	local succ, msg = (hook.Call('cmd.CanRunCommand', nil, caller, cmd, msg, args) or cmd:CanRun(caller, args) or cmd.Callback(caller, args))
+
+	if (succ == false) then
+		hook.Call('cmd.OnCommandError', nil, caller, cmd, msg, args)
+	elseif (succ == nil) or (succ == true) then
+		hook.Call('cmd.OnCommandRun', nil, caller, cmd, msg, args)
 	end
 	return succ, msg
 end
@@ -56,26 +132,67 @@ end
 
 -- Set
 function cmd_mt:SetConCommand(name)
-
+	name = name:lower()
+	self.ConCommand = name
+	if (not concommands[name]) then
+		concommands[name] = true
+		if (SERVER) then
+			concommand.Add(name, function(pl, cmd, args, str)
+				if (not args[1]) then 
+					cmd = args[1]
+					table.remove(args, 1)
+					for i = 1, #args do
+						if (string.upper(tostring(v)) == 'STEAM_0') and (args[i + 4]) then
+							args[i] = table.concat(args, '', i, i + 4)
+							for _ = 1, 4 do
+								table.remove(args, i + 1)
+							end
+							break
+						end
+					end
+					cmd.Run(pl, cmd, args)
+				end
+			end)
+		else
+			concommand.Add('_' .. name, function(pl, cmd, args, str)
+				LocalPlayer():ConCommand(name .. ' ' .. str)
+			end, function(cmd, str)
+				-- Auto complete
+			end)
+		end
+	end
+	return self
 end
 
-function cmd_mt:AddParam(key, type, opts)
-
+function cmd_mt:AddParam(type, opts)
+	self.Params[#self.Params + 1] = {
+		Type = type,
+		Opts = opts
+	}
+	return self
 end
 
 function cmd_mt:RunOnClient(callback)
-
+	if (CLIENT) then
+		self.ClientCallback = callback
+	end
+	return self
 end
+
 
 -- Get
 function cmd_mt:GetName()
-
+	return self.Name
 end
 
 function cmd_mt:GetNiceName()
-
+	return self.NiceName
 end
 
 function cmd_mt:GetConCommand()
+	return self.ConCommand
+end
 
+function cmd_mt:GetParams()
+	return self.Params
 end
