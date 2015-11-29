16cf38be32278f58036b9c3fada4e2c7417e5ea9
aStonedPenguin <zach@superiorservers.co>
2015-11-28 21:10:44 -0900

    docs

--- lua/plib/libraries/nw.lua
+++ lua/plib/libraries/nw.lua
@@ -1,3 +1,26 @@
+--[[
+local var = nw.Register 'MyVar' -- You MUST call this ALL shared
+	var:Write(net.WriteUInt, 32) -- Write function
+	var:Read(net.ReadUInt, 32) -- Read function
+	var:SetLocal() -- Optionally set the var to only network to the player its set on
+	var:SetGlobal() -- Registers the var for use with nw.SetGlobal
+	var:SetNoSync() -- Stops the var from syncing to new players, SetLocal does this for you.
+	var:Filter(function(ent, value) -- Sets a var to only send to players you return in your callback
+		return player.GetWhatever() -- return table players
+	end)
+
+nw.WaitForPlayer(player, callback) -- Calls your callback when the player is ready to recieve net messages
+
+-- Set Functions
+ENTITY:SetNetVar(var, value)
+nw.SetGlobal(var, value)
+
+-- Get functions
+ENTITY:GetNetVar(var)
+nw.GetGlobal(var)
+]]
+
+
 nw 				= {}
 
 local vars 		= {}
