e2b52b78423f1a16ee834b69d0422088394b6cc5
code_gs <collinharris509@icloud.com>
2015-12-22 00:19:24 -0600

    URLEncode/Decode

--- lua/plib/extensions/string.lua
+++ lua/plib/extensions/string.lua
@@ -1,15 +1,15 @@
 function string.Apostrophe(str)
-	local len = string.len(str)
+	local len = str:len()
 	
-	if (string.lower(string.sub(str, len, len)) == "s") then
+	if (str:sub(len, len):lower() == "s") then
 		return "\'"
 	else
 		return "\'s"
 	end
 end
 
-function string.AOrAn(s)
-	return string.match(s, "^h?[AaEeIiOoUu]") and "an" or "a"
+function string.AOrAn(str)
+	return str:match("^h?[AaEeIiOoUu]") and "an" or "a"
 end
 
 function string.Random(chars)
@@ -28,12 +28,12 @@ function string.ExplodeQuotes(str) -- Re-do this one of these days
 	str = ' ' .. str .. ' '
 	local res = {}
 	local ind = 1
-	while (true) do
-		local sInd, start = string.find(str, '[^%s]', ind)
+	while true do
+		local sInd, start = str:find('[^%s]', ind)
 		if not sInd then break end
 		ind = sInd + 1
 		local quoted = str:sub(sInd, sInd):match('["\']') and true or false
-		local fInd, finish = string.find(str, quoted and '["\']' or '[%s]', ind)
+		local fInd, finish = str:find(quoted and '["\']' or '[%s]', ind)
 		if not fInd then break end
 		ind = fInd + 1
 		local str = str:sub(quoted and sInd + 1 or sInd, fInd - 1)
@@ -43,5 +43,20 @@ function string.ExplodeQuotes(str) -- Re-do this one of these days
 end
 
 function string.IsSteamID(str)
-	return str:match('^STEAM_%d:%d:%d+$')
+	return str:match("^STEAM_%d:%d:%d+$")
+end
+
+local formatHex = "%%%02X"
+
+function string.URLEncode(str)
+	return str:gsub("([^%w%-%_%.%~])", function( hex ) 
+		return formatHex:format( hex:byte() ) 
+	end )
+end
+
+function util.URLDecode(str)
+	return str:gsub( "+", " " ):gsub( "%%(%x%x)", function( hex )
+		return string.char( tonumber( hex, 16 ) )
+	end )
+	return r
 end
