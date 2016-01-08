e4760e8cc686a5953f47b3a7904e575758964a65
aStonedPenguin <zach@superiorservers.co>
2016-01-08 00:07:30 -0900

    cvar lib updates

--- lua/plib/libraries/client/cvar.lua
+++ lua/plib/libraries/client/cvar.lua
@@ -1,33 +1,99 @@
-require('pon') -- todo, maybe make this use sqlite someday
+require 'pon1'
 
-local file 			= file
-local hook 			= hook
-local pon 			= pon
-
-cvar 				= {}
-local cvar_file 	= 'plib_cvars.txt'
-local suc, ret 		= pcall(pon.decode, file.Read(cvar_file, 'DATA'))
-local stored_vars 	= suc and ret or {}
-
-function cvar.Create(name, default_value, callback) -- This is optional
-	if (stored_vars[name] == nil) then
-		stored_vars[name] =  default_value
+cvar = setmetatable({}, {
+	__call = function(self, ...)
+		return self.Register(...)
 	end
-	if callback then
-		cvar.AddCallback(name, callback)
+})
+cvar.GetTable = setmetatable({}, {
+	__call = function(self)
+		return self
+	end
+})
+
+local cvar_mt 	= {}
+cvar_mt.__index = cvar_mt
+
+local function encode(data)
+	return util.Compress(pon1.encode(data))
+end
+
+local function decode(data)
+	return pon1.dencode(util.Decompress(data))
+end
+
+local function load()
+	if (not file.IsDir('cvar', 'DATA')) then
+		file.CreateDir('cvar')
+	else
+		local files, _ = file.Find('*.dat', 'cvar', 'DATA')
+		for k, v in ipairs(files) do
+			local c = setmetatable({decode(file.Read('cvar/' .. v, 'DATA'))}, cvar_mt)
+			cvar.GetTable[c.Name] = c
+		end
 	end
 end
 
-function cvar.AddCallback(name, callback)
-	return hook.Add('cvar.' .. name, callback)
+function cvar_mt:SetValue(value)
+	hook.Call('cvar.' ..  self.Name, nil, self.Value, value)
+	self.Value = value
+	file.Write('cvar/' .. self.ID .. '.dat', encode(self))
+	return self
 end
 
-function cvar.Set(name, value)
-	stored_vars[name] = value
-	hook.Call('cvar.' ..  name, nil, value)
-	file.Write(cvar_file, pon.encode(stored_vars))
+function cvar_mt:SetDefault(value)
+	if (not self.Value) then
+		self.Value = value
+	end
+	return self
+end
+
+function cvar_mt:AddMetadata(key, value)
+	self.Metadata[key] = value
+	return self
+end
+
+function cvar_mt:AddCallback(callback)
+	hook.Add('cvar.' .. self.Name, callback)
+	return self
+end
+
+function cvar_mt:GetName()
+	return self.Name
+end
+
+function cvar_mt:GetValue()
+	return self.Value
+end
+
+function cvar_mt:GetMetadata(key)
+	return self.Metadata[key]
+end
+
+function cvar.Register(name)
+	if (not cvar.GetTable[name]) then
+		cvar.GetTable[name] = setmetatable({
+			Name = name,
+			ID 	= util.CRC(name),
+			Metadata = {}
+		}, cvar_mt)
+	end
+	return cvar.GetTable[name]
 end
 
 function cvar.Get(name)
-	return stored_vars[name]
+	if (not cvar.GetTable[name]) then
+		cvar.Register(name)
+	end
+	return cvar.GetTable[name]
 end
+
+function cvar.SetValue(name, value)
+	cvar.Get(name):SetValue(value)
+end
+
+function cvar.GetValue(name)
+	return (cvar.GetTable[name] ~= nil) and cvar.GetTable[name].Value
+end
+
+load()
